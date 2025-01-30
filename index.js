526381225dccf90bdb3965db1c08ae14655599e8
singhsubm <singhsubm24@gamil.com>
2025-01-31 02:59:05 +0530

    final

--- index.js
+++ index.js
@@ -62,4 +62,7 @@ app.get('/read/:title/:id', async (req, res) => {
     res.render('note', { note })
 })
 
-app.listen(5000)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
